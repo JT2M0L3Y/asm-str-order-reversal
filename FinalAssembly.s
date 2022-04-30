9783fd74401454a8a3af6717d23e62acfca8ea1d
Manca <mmanca2@zagmail.gonzaga.edu>
2022-04-30 16:42:56 -0700

    messing with initial loop

--- FinalAssembly.s
+++ FinalAssembly.s
@@ -1,11 +1,11 @@
 
-    This program takes in input of a sentence, and then prints
-    it in reverse order.
+#This program takes in input of a sentence, and then prints
+#it in reverse order.
 
 LC0:
     .ascii "Enter in any sentence: \0"
 LC1:
-    .ascii %[^\n]\0
+    .ascii "%[^\n]\0"
 
 
     .globl _main
@@ -16,11 +16,23 @@ _main:
     pushl   %ebp            # Standard Call entrance
     movl    %esp, %ebp      # moving the base pointer to top of stack pointer
 
-    pushl   %ebp
+    pushl   %ebp            # save value of esp inside of the stack
 
-    movl    $LC0, (%esp)
-    call    _printf
+    movl    $LC0, (%esp)    # Asks user for input
+    call    _printf         # prints Enter in any sentence
+
+    movl    $LC1, (%esp)    # gets input from the user
+    call    _scanf          
+
+    movl    $0, %ebx        # move 0 to ebx for incrementing
+    jmp     L2              # jump to loop comparison
+
+
+L3:
 
 
 
+L2:
+    li      $t1, 10
+    beq     $t0, $t1, L3
 
